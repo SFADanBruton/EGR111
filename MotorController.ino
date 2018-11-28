eede7719b4cc2277969ef2c00312c424c99ecf5a
SFADanBruton <45438241+SFADanBruton@users.noreply.github.com>
2018-11-28 14:58:09 -0600

    Add files via upload

---
+++ MotorController.ino
@@ -0,0 +1,115 @@
+/*
+SFA Example Motor Code
+*/
+
+//the left motor will be controlled by the motor B pins on the motor driver
+const int PWMB = 10;           //speed control pin on the motor driver for the left motor
+const int BIN2 = 9;           //control pin 2 on the motor driver for the left motor
+const int BIN1 = 8;           //control pin 1 on the motor driver for the left motor
+
+//the right motor will be controlled by the motor A pins on the motor driver
+const int AIN1 = 13;           //control pin 1 on the motor driver for the right motor
+const int AIN2 = 12;            //control pin 2 on the motor driver for the right motor
+const int PWMA = 11;            //speed control pin on the motor driver for the right motor
+
+int switchPin = 7;             //switch to turn the robot on and off
+
+float temperature = 0;            //variable to store the temperature from the temperature sensor
+
+//robot behaviour variables
+int backupTime = 300;           //amount of time that the robot will back up when it senses an object
+int turnTime = 200;             //amount that the robot will turn once it has backed up
+
+/********************************************************************************/
+void setup()
+{
+  pinMode(trigPin, OUTPUT);       //this pin will send ultrasonic pulses out from the distance sensor
+  pinMode(echoPin, INPUT);        //this pin will sense when the pulses reflect back to the distance sensor
+
+  pinMode(switchPin, INPUT_PULLUP);   //set this as a pullup to sense whether the switch is flipped
+
+  //set the motor contro pins as outputs
+  pinMode(AIN1, OUTPUT);
+  pinMode(AIN2, OUTPUT);
+  pinMode(PWMA, OUTPUT);
+
+  pinMode(BIN1, OUTPUT);
+  pinMode(BIN2, OUTPUT);
+  pinMode(PWMB, OUTPUT);
+
+  Serial.begin(9600);        //begin serial communication with the computer
+  Serial.print("Test");     //test the serial connection
+}
+
+/********************************************************************************/
+void loop()
+{
+  
+      //stop for a moment
+      rightMotor(0);
+      leftMotor(0);
+      delay(1000);
+
+      //test move forward
+      rightMotor(100);
+      leftMotor(100);
+      delay(1000);
+
+      //test back up
+      rightMotor(-100);
+      leftMotor(-100);
+      delay(1000);
+      
+      //test turn to the right
+      rightMotor(-100);
+      leftMotor(100);    
+      delay(turnTime);
+
+      //test turn to the left
+      rightMotor(100);
+      leftMotor(-100);    
+      delay(turnTime);
+
+}
+
+/********************************************************************************/
+void rightMotor(int motorSpeed)                       //function for driving the right motor
+{
+  if (motorSpeed > 0)                                 //if the motor should drive forward (positive speed)
+  {
+    digitalWrite(AIN1, HIGH);                         //set pin 1 to high
+    digitalWrite(AIN2, LOW);                          //set pin 2 to low
+  }
+  else if (motorSpeed < 0)                            //if the motor should drive backwar (negative speed)
+  {
+    digitalWrite(AIN1, LOW);                          //set pin 1 to low
+    digitalWrite(AIN2, HIGH);                         //set pin 2 to high
+  }
+  else                                                //if the motor should stop
+  {
+    digitalWrite(AIN1, LOW);                          //set pin 1 to low
+    digitalWrite(AIN2, LOW);                          //set pin 2 to low
+  }
+  analogWrite(PWMA, abs(motorSpeed));                 //now that the motor direction is set, drive it at the entered speed
+}
+
+/********************************************************************************/
+void leftMotor(int motorSpeed)                        //function for driving the left motor
+{
+  if (motorSpeed > 0)                                 //if the motor should drive forward (positive speed)
+  {
+    digitalWrite(BIN1, HIGH);                         //set pin 1 to high
+    digitalWrite(BIN2, LOW);                          //set pin 2 to low
+  }
+  else if (motorSpeed < 0)                            //if the motor should drive backwar (negative speed)
+  {
+    digitalWrite(BIN1, LOW);                          //set pin 1 to low
+    digitalWrite(BIN2, HIGH);                         //set pin 2 to high
+  }
+  else                                                //if the motor should stop
+  {
+    digitalWrite(BIN1, LOW);                          //set pin 1 to low
+    digitalWrite(BIN2, LOW);                          //set pin 2 to low
+  }
+  analogWrite(PWMB, abs(motorSpeed));                 //now that the motor direction is set, drive it at the entered speed
+}
